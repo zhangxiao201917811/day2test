bc2ca25e8e34c701ca5151275088f9394532c665
张霄 <1537284929@qq.com>
2022-06-08 15:17:30 +0800

    第6次提交，提交了push

--- src/test.java
+++ src/test.java
@@ -1,7 +1,6 @@
 public class test {
     public static void main(String[] args){
         System.out.println("master");
-
         System.out.println("dev");
         System.out.println("push");
     }
